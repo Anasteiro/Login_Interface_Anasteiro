8a5d85eeec3a601fbb562b82b3c4a737555344f8
Anasteiro <68961754+Anasteiro@users.noreply.github.com>
2024-08-13 02:25:10 +0300

    V2.0 ADDED MYSQL

--- main.py
+++ main.py
@@ -1,61 +1,73 @@
 import time
 import os
-import json
-database = {}
+import mysql.connector
 
 
-def load():   # SHOULD BE RUN ONLY ONCE AT THE START OF THE PROGRAM TO PARSE THE DATA FROM THE JSON FILE IN THE DATABASE
-    global database
-    with open("database.json", "r") as file:
-        database = json.load(file)
+conn = mysql.connector.connect(
+    username='root',
+    password='password',
+    host='localhost'
+)
+
+cursor = conn.cursor()
+
+cursor.execute('CREATE DATABASE IF NOT EXISTS login')
+conn.commit()
+cursor.execute('USE login')
+cursor.execute('CREATE TABLE IF NOT EXISTS creds(id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, username varchar(15) NOT NULL, password varchar(15) NOT NULL)')
+conn.commit()
+
+def main_query(username, password):
+    cursor.execute('SELECT * FROM creds WHERE username = %s AND password = %s', (username, password))
+    results = cursor.fetchone()
+    return results
+
+def clear_console():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
 
 
-def save():  # SHOULD BE RUN TO SAVE THE ENTIRE DATABASE AS IT IS IN THE TXT WARNING: IT WILL REPLACE ALL DATA THERE WITH WHAT'S IN DATABASE
-    with open("database.json", "w") as file:
-        json.dump(database, file, indent=4)
 
 
 def reset():
     while True:
         reset = input("Do you wish to reset the password of your account: y/n\n=> ")
         time.sleep(2)
-        if reset in ["y", "Y", "Yes", "YES", "yes"]:
-            os.system("cls")
+        if reset.lower() in ["y", "yes"]:
+            clear_console()
             username = input("Enter the username you want to reset the password of: ")
             old_password = input("Enter your current password: ")
-            os.system("cls")
-            if username in database:
-                if database[username] == old_password:
+            clear_console()
+            user_creds = main_query(username, old_password)
+            if user_creds:
                     new_pass = input("Enter your new password: ")
                     confirm_new_pass = input("Confirm new password: ")
                     if new_pass == confirm_new_pass:
-                        database[username] = new_pass
-                        save()
-
+                        cursor.execute('UPDATE creds SET password = %s WHERE username = %s', (new_pass, username))
                         print(f"Password for {username} has been succesfully reseted!")
+                        conn.commit()
                         time.sleep(2)
                         break
                     else:
                         print("passwords do not match please try again...")
                         time.sleep(2)
-                        os.system("cls")
-                else:
-                    print("Incorrect password...Please try again..")
-                    time.sleep(2)
-                    os.system("cls")
+                        clear_console()
+
             else:
-                print("Username has not been found in database")
+                print("Username or password has not been found in the database")
                 time.sleep(2)
-                os.system("cls")
-        elif reset in ["N", "n", "No", "NO", "no"]:
+                clear_console()
+        elif reset.lower() in ["n", "no"]:
             print("Password reset has been cancelled")
             time.sleep(2)
-            os.system("cls")
+            clear_console()
             break
         else:
             print(f"Wrong input, {reset} is not a valid choice")
             time.sleep(2)
-            os.system("cls")
+            clear_console()
 
 
 def login():
@@ -76,22 +88,17 @@ def login():
         name = input("Enter your username: ")
         password = input("Enter your password: ")
         time.sleep(2)
-        if name in database:
-            if database[name] == password:
-                os.system("cls")
-                print(f"Welcome {name}")
-                time.sleep(2)
-                os.system("cls")
-                break
-            else:
-                print("Username or Password wrong")
-                time.sleep(2)
-                os.system("cls")
-                tries += 1
+        user_creds = main_query(name, password)
+        if user_creds:
+            clear_console()
+            print(f"Welcome {name}")
+            time.sleep(2)
+            clear_console()
+            break
         else:
             print("Username or Password wrong")
             time.sleep(2)
-            os.system("cls")
+            clear_console()
             tries += 1
 
 
@@ -99,36 +106,39 @@ def login():
 def add():
     new_user = input("Enter a new username: ")
     new_password = input("Enter a new password: ")
-    if new_user in database:
+    user_creds = main_query(new_user, new_password)
+    if user_creds:
         print("Username already exists...")
         time.sleep(2)
     else:
-        database[new_user] = new_password
-        save()
+        cursor.execute('INSERT INTO creds(username, password) VALUES(%s, %s)', (new_user, new_password))
+        conn.commit()
         print(f"New account: {new_user} succesfully added!")
 
 
-def pop(account):
-    if account in database:
-        database.pop(account)
-        save()
-        print(f"{account} succesfully deleted!")
+def pop(username):
+    cursor.execute('SELECT * FROM creds WHERE username = %s', (username,))
+    user_creds = cursor.fetchone()
+    if user_creds:
+        cursor.execute('DELETE FROM creds WHERE username = %s', (username,))
+        conn.commit()
+        print(f"{username} succesfully deleted!")
         time.sleep(2)
-        os.system("cls")
+        clear_console()
 
 
 
 def menu():
     while True:
-        print("...Digital Login Interface V.1.0 By Anasteiro...\n")
+        print("...Digital Login Interface V.2.0 By Anasteiro...\n")
         time.sleep(2)
         option = input("Options:\n1)Add a new account on the database\n2)Delete your account\n3)Logout\n4)Check your Account\n5)Exit\n=> ")
         if option == "1":
             add()
-            os.system("cls")
+            clear_console()
             time.sleep(2)
         elif option == "2":
-            os.system("cls")
+            clear_console()
             pop(name)
             time.sleep(2)
             in_or_up = input("Would you like to sign in or sign up?(type in if you have an account or up to create one)\n=> ")
@@ -136,19 +146,24 @@ def menu():
                 login()
             elif in_or_up.lower() == "up":
                 add()
-                os.system("cls")
+                clear_console()
                 login()
             else:
                 print("Wrong Input...")
+                time.sleep(1)
+                clear_console()
 
         elif option == "3":
-            os.system("cls")
+            clear_console()
 
             login()
 
         elif option == "4":
             print(f"Username: {name}")
-            print("password: " + len(database[name])*"*")
+            cursor.execute('SELECT password FROM creds WHERE username = %s', (name, ))
+            tup_pass = cursor.fetchone()
+            password = tup_pass[0]
+            print("password: " + len(password)*"*")
             time.sleep(3)
             reset()
 
@@ -156,38 +171,35 @@ def menu():
             print("See you soon!")
             break
         elif option == "hack":
-            for value, key in database.items():
-                print(f"USERNAME:{value} PASSWORD:{key}")
-                time.sleep(1)
+            cursor.execute('SELECT * FROM creds')
+            user_creds = cursor.fetchall()
+            print(user_creds)
+            time.sleep(1)
         else:
             print(f"Wrong input, {option} is not a valid choice")
             time.sleep(2)
-            os.system("cls")
+            clear_console()
 
 try:
-    try:
-        load()
-    except FileNotFoundError:
-        print("Creating database file...")
-        time.sleep(2)
-        with open("database.json", "w") as file:
-            json.dump(database, file, indent=4)
     while True:
-        os.system("cls")
+        clear_console()
         in_up = input("Hello, welcome to the digital login interface by Anasteiro, do you have an account?(y/n)\n=> ")
         if in_up.lower() in ["y", "yes"]:
             login()
             menu()
         elif in_up.lower() in ["n", "no"]:
             add()
-            os.system("cls")
+            clear_console()
             time.sleep(2)
             login()
             menu()
         else:
             print(f"Wrong input, {in_up} is not a valid choice")
             time.sleep(2)
-            os.system("cls")
+            clear_console()
 
 except KeyboardInterrupt:
     print("\nProgram succesfully stopped by user keyboard interruption...")
+finally:
+    cursor.close()
+    conn.close()
